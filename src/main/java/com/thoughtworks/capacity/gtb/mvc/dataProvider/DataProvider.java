f9207835c51da238e97808862e27deb30f31e16a
GraceeFlower <1491644103@qq.com>
2020-10-20 10:05:39 +0800

    [Min] feat: add Data Provider

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/dataProvider/DataProvider.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.capacity.gtb.mvc.dataProvider;
+
+import com.thoughtworks.capacity.gtb.mvc.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataProvider {
+    static List<User> userList = new ArrayList<>();
+}
