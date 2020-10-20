5cd154fa48be226b3ce13a94a10d31f7a0ddcbdd
GraceeFlower <1491644103@qq.com>
2020-10-20 16:07:46 +0800

    [Min] refactor: enable lombok

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/ErrorResult.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/ErrorResult.java
@@ -2,35 +2,19 @@ package com.thoughtworks.capacity.gtb.mvc.exception;
 
 import org.springframework.http.HttpStatus;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResult {
 
     private String message;
     private HttpStatus code = HttpStatus.BAD_REQUEST;
 
-    public ErrorResult() {
-    }
-
     public ErrorResult(String message) {
-        this.message = message;
-    }
-
-    public ErrorResult(String message, HttpStatus httpStatus) {
-        this.message = message;
-        this.code = httpStatus;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 
@@ -38,7 +22,4 @@ public class ErrorResult {
         return code.toString();
     }
 
-    public void setCode(HttpStatus code) {
-        this.code = code;
-    }
 }
